90edebaefb8b2fca745b38ac43cf8a846c560606
sold666 <korch937@mail.ru>
2023-02-28 01:49:32 +0300

    add loginActivity

---
+++ app/src/main/java/com/example/developers_messenger/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.developers_messenger.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
