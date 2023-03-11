a6be9c8a0db6e2af0f17d994ef073d567247f084
sold666 <korch937@mail.ru>
2023-03-11 21:33:57 +0300

    delete files

--- app/src/main/java/com/example/developers_messenger/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.example.developers_messenger.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
--- app/src/main/java/com/example/developers_messenger/ui/login/LoggedInUserView.kt
+++
@@ -1,9 +0,0 @@
-package com.example.developers_messenger.ui.login
-
-/**
- * User details post authentication that is exposed to the UI
- */
-data class LoggedInUserView(
-    val displayName: String
-    //... other data fields that may be accessible to the UI
-)
