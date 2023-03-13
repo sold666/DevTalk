99e0552d37553cf22bec0be6fbb76ba610c54ba1
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-03-13 04:26:53 +0300

    adding Search

--- app/src/main/java/com/dev_talk/main/recommended_chats/RecommendedChatsAdapter.kt
+++ app/src/main/java/com/dev_talk/main/recommended_chats/RecommendedChatsAdapter.kt
@@ -1,27 +1,38 @@
 package com.dev_talk.main.recommended_chats
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dev_talk.main.R
 import com.dev_talk.main.structures.Chat
+import java.util.*
 
 class RecommendedChatsAdapter(
-    private val chats: List<Chat>
-) : RecyclerView.Adapter<RecommendedChatsAdapter.ChatItemViewHolder>() {
+    private var chats: List<Chat>
+) : RecyclerView.Adapter<RecommendedChatsAdapter.ChatItemViewHolder>(), Filterable {
+
+    internal var filteredChats: List<Chat>
+
+    init {
+        filteredChats = chats
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatItemViewHolder {
         return ChatItemViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.default_chat_item, null)
         )
     }
 
-    override fun getItemCount() = chats.size
+    override fun getItemCount() = filteredChats.size
 
     override fun onBindViewHolder(holder: ChatItemViewHolder, position: Int) {
-        holder.bind(chats[position])
+        holder.bind(filteredChats[position])
     }
 
     class ChatItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -31,6 +42,37 @@ class RecommendedChatsAdapter(
         fun bind(src: Chat) {
             icon.setImageResource(src.icon)
             chat.text = src.tags
+        }
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(p0: CharSequence?): FilterResults {
+                val query = p0.toString()
+                filteredChats = if (query.isEmpty()) {
+                    chats
+                } else {
+                    val resultList = ArrayList<Chat>()
+                    for (chat in chats) {
+                        if (chat.tags.lowercase(Locale.ROOT)
+                                .contains(query.lowercase(Locale.ROOT))
+                        ) {
+                            resultList.add(chat)
+                        }
+                    }
+                    resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = filteredChats
+                return filterResults
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun publishResults(quary: CharSequence?, filterResults: FilterResults?) {
+                filteredChats = filterResults!!.values as List<Chat>
+                notifyDataSetChanged()
+            }
+
         }
     }
 }
--- app/src/main/java/com/dev_talk/main/recommended_chats/RecommendedChatsFragment.kt
+++ app/src/main/java/com/dev_talk/main/recommended_chats/RecommendedChatsFragment.kt
@@ -1,20 +1,23 @@
 package com.dev_talk.main.recommended_chats
 
+
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
 import androidx.core.content.ContextCompat
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dev_talk.main.R
 import com.dev_talk.main.databinding.FragmentRecommendedChatsBinding
 import com.dev_talk.main.structures.Chat
 
+
 private const val RECOMMENDED_LIST_PROFESSIONS_KEY = "recommendations"
 
-class RecommendedChatsFragment : Fragment() {
+class RecommendedChatsFragment : Fragment(), MenuProvider {
     private lateinit var binding: FragmentRecommendedChatsBinding
 
     override fun onCreateView(
@@ -37,15 +40,44 @@ class RecommendedChatsFragment : Fragment() {
                 } else {
                     arguments?.getParcelableArrayList(RECOMMENDED_LIST_PROFESSIONS_KEY)!!
                 }
+            val adapterRV = RecommendedChatsAdapter(data)
             recommendedChats.apply {
-                adapter = RecommendedChatsAdapter(data)
+                adapter = adapterRV
                 layoutManager = LinearLayoutManager(context)
                 addItemDecoration(getRecyclerViewDivider(context))
             }
             searchBar.setOnClickListener {
                 searchBar.onActionViewExpanded()
             }
+            searchBar.isSubmitButtonEnabled = true
+            searchBar.setOnQueryTextListener(
+                object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        searchBar.clearFocus()
+                        adapterRV.filter.filter(query)
+                        return true
+                    }
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        adapterRV.filter.filter(newText)
+                        return true
+                    }
+                }
+            )
         }
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.recommendations_search_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPause() {
+        binding.searchBar.setQuery("", false);
+        super.onPause()
     }
 
     companion object {
@@ -63,9 +95,10 @@ class RecommendedChatsFragment : Fragment() {
         decoration.setDrawable(
             ContextCompat.getDrawable(
                 context,
-                com.dev_talk.main.R.drawable.divider
+                R.drawable.divider
             )!!
         )
         return decoration
     }
+
 }
