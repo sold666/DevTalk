d2191c81a5c5f5672a9edbb52600432fc701cac8
sold666 <korch937@mail.ru>
2023-03-14 15:03:22 +0300

    add design for buttons and delete checkboxes logic

--- app/src/main/java/com/dev_talk/auth/ProfessionAdapter.kt
+++ app/src/main/java/com/dev_talk/auth/ProfessionAdapter.kt
@@ -1,10 +1,12 @@
 package com.dev_talk.auth
 
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.dev_talk.R
 import com.dev_talk.structures.Profession
@@ -17,7 +19,7 @@ class ProfessionAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfessionViewHolder {
         return ProfessionViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_profession, null)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_profession, parent, false)
         )
     }
 
@@ -28,15 +30,36 @@ class ProfessionAdapter(
     override fun onBindViewHolder(holder: ProfessionViewHolder, position: Int) {
         val profession = professions[position]
         holder.bind(profession)
-        holder.professionCheckbox.isChecked = selectedProfessions.contains(profession)
 
-        holder.professionCheckbox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                selectedProfessions.add(profession)
-            } else {
-                selectedProfessions.remove(profession)
-            }
+        val typedValueForBackground = TypedValue()
+        val theme = holder.itemView.context.theme
+        theme.resolveAttribute(R.attr.background_color_primary, typedValueForBackground, true)
+        val typedValueForButton = TypedValue()
+        theme.resolveAttribute(R.attr.button_click_color_secondary, typedValueForButton, true)
+
+        holder.itemView.setOnClickListener {
+            holder.itemView.findViewById<CardView>(R.id.profession_card).setCardBackgroundColor(
+                if (selectedProfessions.contains(profession)) {
+                    selectedProfessions.remove(profession)
+                    ContextCompat.getColor(
+                        holder.itemView.context,
+                        typedValueForBackground.resourceId
+                    )
+                } else {
+                    selectedProfessions.add(profession)
+                    ContextCompat.getColor(holder.itemView.context, typedValueForButton.resourceId)
+                }
+            )
         }
+        holder.itemView.findViewById<CardView>(R.id.profession_card).setCardBackgroundColor(
+            if (selectedProfessions.contains(profession)) ContextCompat.getColor(
+                holder.itemView.context,
+                typedValueForButton.resourceId
+            ) else ContextCompat.getColor(
+                holder.itemView.context,
+                typedValueForBackground.resourceId
+            )
+        )
     }
 
     fun getSelectedProfessions(): ArrayList<Profession> {
@@ -45,7 +68,6 @@ class ProfessionAdapter(
 
     class ProfessionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val professionName: TextView = itemView.findViewById(R.id.profession_name)
-        val professionCheckbox: CheckBox = itemView.findViewById(R.id.profession_checkbox)
 
         fun bind(profession: Profession) {
             professionName.text = profession.name
--- app/src/main/java/com/dev_talk/auth/TagAdapter.kt
+++ app/src/main/java/com/dev_talk/auth/TagAdapter.kt
@@ -1,10 +1,12 @@
 package com.dev_talk.auth
 
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.dev_talk.R
 
@@ -16,7 +18,7 @@ class TagAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TagViewHolder {
         return TagViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_tag, null)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_tag, parent, false)
         )
     }
 
@@ -27,15 +29,36 @@ class TagAdapter(
     override fun onBindViewHolder(holder: TagViewHolder, position: Int) {
         val tag = tags[position]
         holder.bind(tag)
-        holder.tagCheckbox.isChecked = selectedTags.contains(tag)
 
-        holder.tagCheckbox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                selectedTags.add(tag)
-            } else {
-                selectedTags.remove(tag)
-            }
+        val typedValueForBackground = TypedValue()
+        val theme = holder.itemView.context.theme
+        theme.resolveAttribute(R.attr.background_color_primary, typedValueForBackground, true)
+        val typedValueForButton = TypedValue()
+        theme.resolveAttribute(R.attr.button_click_color_secondary, typedValueForButton, true)
+
+        holder.itemView.setOnClickListener {
+            holder.itemView.findViewById<CardView>(R.id.tag_card).setCardBackgroundColor(
+                if (selectedTags.contains(tag)) {
+                    selectedTags.remove(tag)
+                    ContextCompat.getColor(
+                        holder.itemView.context,
+                        typedValueForBackground.resourceId
+                    )
+                } else {
+                    selectedTags.add(tag)
+                    ContextCompat.getColor(holder.itemView.context, typedValueForButton.resourceId)
+                }
+            )
         }
+        holder.itemView.findViewById<CardView>(R.id.tag_card).setCardBackgroundColor(
+            if (selectedTags.contains(tag)) ContextCompat.getColor(
+                holder.itemView.context,
+                typedValueForButton.resourceId
+            ) else ContextCompat.getColor(
+                holder.itemView.context,
+                typedValueForBackground.resourceId
+            )
+        )
     }
 
     fun getSelectedTags(): ArrayList<String> {
@@ -44,7 +67,6 @@ class TagAdapter(
 
     class TagViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val tagName: TextView = itemView.findViewById(R.id.tag_name)
-        val tagCheckbox: CheckBox = itemView.findViewById(R.id.tag_checkbox)
 
         fun bind(tag: String) {
             tagName.text = tag
