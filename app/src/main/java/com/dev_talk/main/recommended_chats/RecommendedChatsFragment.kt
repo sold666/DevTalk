3d7e49cba1e6ac4e1eca19042f6e5726a8c0a37b
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-03-17 01:13:42 +0300

    fixing search

--- app/src/main/java/com/dev_talk/main/recommended_chats/RecommendedChatsFragment.kt
+++ app/src/main/java/com/dev_talk/main/recommended_chats/RecommendedChatsFragment.kt
@@ -6,8 +6,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.view.size
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,6 +24,7 @@ private const val RECOMMENDED_LIST_PROFESSIONS_KEY = "recommendations"
 class RecommendedChatsFragment : Fragment() {
     private lateinit var binding: FragmentRecommendedChatsBinding
     private lateinit var adapterRV: RecommendedChatsAdapter
+    private lateinit var dataRV: List<Chat>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,7 +37,7 @@ class RecommendedChatsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
-            val data =
+            dataRV =
                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
                     arguments?.getParcelableArrayList(
                         RECOMMENDED_LIST_PROFESSIONS_KEY,
@@ -42,7 +46,7 @@ class RecommendedChatsFragment : Fragment() {
                 } else {
                     arguments?.getParcelableArrayList(RECOMMENDED_LIST_PROFESSIONS_KEY)!!
                 }
-            adapterRV = RecommendedChatsAdapter(data)
+            adapterRV = RecommendedChatsAdapter(dataRV)
             recommendedChats.apply {
                 adapter = adapterRV
                 layoutManager = LinearLayoutManager(context)
@@ -75,24 +79,23 @@ class RecommendedChatsFragment : Fragment() {
     }
 
     private fun setUpSearchView() {
-
         val searchView = binding.searchBar.menu.findItem(R.id.menu_search)?.actionView as SearchView
         with(searchView) {
-
+            maxWidth = Integer.MAX_VALUE;
             queryHint = getString(R.string.default_query_hint)
             setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     clearFocus()
-                    setQuery("", false)
                     binding.searchBar.menu.findItem(R.id.menu_search)?.collapseActionView()
                     adapterRV.filter.filter(query)
+                    if (binding.recommendedChats.size == 0) {
+                        adapterRV.filter.filter("")
+                    }
                     return true
                 }
 
                 override fun onQueryTextChange(query: String?): Boolean {
                     adapterRV.filter.filter(query)
-
-
                     return true
                 }
             })
