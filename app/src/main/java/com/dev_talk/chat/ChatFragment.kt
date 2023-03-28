beadb7b57d38508945229558af0e23b0f21ee232
sold666 <korch937@mail.ru>
2023-03-29 00:01:32 +0300

    fix CG

--- app/src/main/java/com/dev_talk/chat/ChatFragment.kt
+++ app/src/main/java/com/dev_talk/chat/ChatFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.developers_messenger.R
-import com.example.developers_messenger.databinding.ChatFragmentBinding
+import com.dev_talk.R
+import com.dev_talk.databinding.ChatFragmentBinding
 
 class ChatFragment : Fragment() {
 
