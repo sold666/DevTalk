579cede6f8f041b554de4ab9196ce5d7b63f8b73
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-04-20 01:54:21 +0300

    add test

--- app/src/androidTest/java/com/dev_talk/RecommendedChatsFragmentTest.kt
+++ app/src/androidTest/java/com/dev_talk/RecommendedChatsFragmentTest.kt
@@ -3,21 +3,26 @@ package com.dev_talk
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.dev_talk.main.recommended_chats.RecommendedChatsFragment
 import junit.framework.TestCase
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.platform.app.InstrumentationRegistry
-import com.dev_talk.main.recommended_chats.RecommendedChatsFragment
 
-
+//Emelyanov Pavel
 @RunWith(AndroidJUnit4::class)
 class RecommendedChatsFragmentTest : TestCase() {
+    companion object {
+        private const val quary = "py"
+    }
+
     private lateinit var scenario: FragmentScenario<RecommendedChatsFragment>
 
     @Before
@@ -28,8 +33,10 @@ class RecommendedChatsFragmentTest : TestCase() {
 
     @Test
     fun testSearchInRecommendations() {
-        onView(withId(R.id.menu_search)).perform(click())
-        assertTrue(true)
+        onView(withId(R.id.menu_search)).perform(click()).perform(typeText(quary))
+        onView(withSubstring(quary)).check(matches(isDisplayed()))
+
+
     }
 
 }
