e86f08256fe2cfb9a8e89932ab58b0d2bd4e639f
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-04-21 02:03:57 +0300

    add test

--- app/src/androidTest/java/com/dev_talk/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/dev_talk/main/MainActivityTest.kt
@@ -1,29 +1,29 @@
 package com.dev_talk.main
 
-import androidx.lifecycle.Lifecycle
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.rule.ActivityTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.dev_talk.R
-import org.junit.Assert.*
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 //Emelyanov Pavel
 class MainActivityTest {
-    @Rule
-    var activityActivityTestRule = ActivityTestRule(
+    @get:Rule
+    var activityActivityTestRule = ActivityScenarioRule(
         MainActivity::class.java
     )
 
     @Test
-    fun clickButtonHome() {
+    fun bottomNavigationTest() {
         onView(withId(R.id.profile_fragment_container)).perform(click())
         onView(withId(R.id.name)).check(matches(isDisplayed()))
+        onView(withId(R.id.recommended_chats_fragment_container)).perform(click())
+        onView(withId(R.id.recommended_chats)).check(matches(isDisplayed()))
+        onView(withId(R.id.personal_chats_fragment_container)).perform(click())
+        onView(withId(R.id.chats_with_category)).check(matches(isDisplayed()))
     }
 }
--- app/src/androidTest/java/com/dev_talk/main/recommended_chats/RecommendedChatsFragmentTest.kt
+++ app/src/androidTest/java/com/dev_talk/main/recommended_chats/RecommendedChatsFragmentTest.kt
@@ -1,8 +1,6 @@
 package com.dev_talk.main.recommended_chats
 
-import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.lifecycle.Lifecycle
+import android.widget.AutoCompleteTextView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
@@ -10,9 +8,8 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.dev_talk.main.MainActivity
 import com.dev_talk.R
-import junit.framework.TestCase
+import com.dev_talk.main.MainActivity
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -21,26 +18,25 @@ import org.junit.runner.RunWith
 
 //Emelyanov Pavel
 @RunWith(AndroidJUnit4::class)
-class RecommendedChatsFragmentTest : TestCase() {
+class RecommendedChatsFragmentTest {
     companion object {
-        private const val quary = "py"
+        private const val QUERY = "py"
     }
 
     @get:Rule
     val activityScenario = ActivityScenarioRule(MainActivity::class.java)
 
-    private lateinit var scenario: FragmentScenario<RecommendedChatsFragment>
-
     @Before
-    override fun setUp() {
-        scenario = launchFragmentInContainer()
-        scenario.moveToState(Lifecycle.State.STARTED)
+    fun setUp() {
+        onView(withId(R.id.recommended_chats_fragment_container)).perform(click())
+        onView(withId(R.id.recommended_chats)).check(matches(isDisplayed()))
     }
 
     @Test
     fun testSearchInRecommendations() {
-        onView(withId(R.id.menu_search)).perform(click()).perform(typeText(quary))
-        onView(withSubstring(quary)).check(matches(isDisplayed()))
+        onView(withId(R.id.menu_search)).perform(click())
+        onView(isAssignableFrom(AutoCompleteTextView::class.java)).perform(typeText(QUERY))
+        onView(withSubstring(QUERY)).check(matches(isDisplayed()))
     }
-
 }
+
