bcd1d7a06a6ecf53eb1ea125383df8fb1f05bfaf
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-04-27 14:58:41 +0300

    fix positions

--- app/src/androidTest/java/com/dev_talk/main/tests/RecommendedChatsFragmentTest.kt
+++ app/src/androidTest/java/com/dev_talk/main/tests/RecommendedChatsFragmentTest.kt
@@ -1,14 +1,10 @@
-package com.dev_talk.main.recommended_chats
+package com.dev_talk.main.tests
 
-import android.widget.AutoCompleteTextView
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.dev_talk.R
 import com.dev_talk.main.MainActivity
 import com.dev_talk.main.screens.MainActivityScreen
 import com.dev_talk.main.screens.RecommendedChatsFragmentScreen
