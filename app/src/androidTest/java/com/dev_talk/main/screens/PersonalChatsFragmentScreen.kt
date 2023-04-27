056e0d6ec116da37adf8159b9089e1d68538226d
Kirill06344 <kirill12093@mail.ru>
2023-04-27 17:41:46 +0300

    add test for Personal Chats

---
+++ app/src/androidTest/java/com/dev_talk/main/screens/PersonalChatsFragmentScreen.kt
@@ -0,0 +1,55 @@
+package com.dev_talk.main.screens
+
+import android.util.Log
+import android.view.View
+import androidx.core.view.size
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.dev_talk.R
+import com.google.android.material.tabs.TabLayout
+import junit.framework.TestCase.assertEquals
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.any
+import java.util.EnumSet.allOf
+import kotlin.math.log
+
+//Bazhenov Kirill
+class PersonalChatsFragmentScreen() {
+
+    init {
+        check()
+    }
+
+    private fun check() {
+        onView(withId(R.id.professions))
+            .check(matches(isDisplayed()))
+    }
+
+    public fun performProfessionWithIndex(index: Int) {
+        onView(withId(R.id.professions)).perform(selectTabAtPosition(index))
+    }
+
+    private fun selectTabAtPosition(tabIndex: Int): ViewAction {
+        return object : ViewAction {
+            override fun getDescription() = "with tab at index $tabIndex"
+            override fun getConstraints(): Matcher<View> {
+                return any(View::class.java)
+            }
+
+            override fun perform(uiController: UiController, view: View) {
+                val tabLayout = view as TabLayout
+                val tabAtIndex: TabLayout.Tab = tabLayout.getTabAt(tabIndex)
+                    ?: throw PerformException.Builder()
+                        .withCause(Throwable("No tab at index $tabIndex"))
+                        .build()
+                tabAtIndex.select()
+            }
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/dev_talk/main/tests/PersonalChatsFragmentTest.kt
@@ -0,0 +1,38 @@
+package com.dev_talk.main.tests
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.dev_talk.main.MainActivity
+import com.dev_talk.main.screens.MainActivityScreen
+import com.dev_talk.main.screens.PersonalChatsFragmentScreen
+import com.dev_talk.main.screens.RecommendedChatsFragmentScreen
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+//Bazhenov Kirill
+@RunWith(AndroidJUnit4::class)
+class PersonalChatsFragmentTest {
+
+    private lateinit var mainActivityScreen: MainActivityScreen
+    private lateinit var personalChatsFragmentScreen: PersonalChatsFragmentScreen
+
+    @get:Rule
+    val activityScenario = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        mainActivityScreen = MainActivityScreen()
+        mainActivityScreen.openPersonalChats()
+        personalChatsFragmentScreen = PersonalChatsFragmentScreen()
+    }
+
+    @Test
+    fun checkWorkingTabs() {
+        for (i in 0..2) {
+            personalChatsFragmentScreen.performProfessionWithIndex(i)
+        }
+    }
+
+}
