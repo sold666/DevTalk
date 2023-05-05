1cdf78fd203b5f9dcfee8d365d5e5012309caca2
Kirill06344 <kirill12093@mail.ru>
2023-05-06 00:38:24 +0300

    fix auth authtorization

--- app/src/main/java/com/dev_talk/splash/SplashActivity.kt
+++ app/src/main/java/com/dev_talk/splash/SplashActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dev_talk.R
 import com.dev_talk.auth.AuthActivity
+import com.dev_talk.main.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -13,9 +15,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
         window.decorView.postDelayed({
-            startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
+            val user = FirebaseAuth.getInstance().currentUser
+            if (user != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this, AuthActivity::class.java))
+            }
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }, SPLASH_DELAY_TIME)
