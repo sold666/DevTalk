6e3718ea3e7642b7e526620908c1f33b0b6ec44c
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-05-12 04:13:31 +0300

    fix string structure

--- app/src/main/java/com/dev_talk/main/structures/Link.kt
+++ app/src/main/java/com/dev_talk/main/structures/Link.kt
@@ -3,5 +3,6 @@ package com.dev_talk.main.structures
 import androidx.annotation.DrawableRes
 
 data class Link(
-    @DrawableRes val icon: Int
+    @DrawableRes val icon: Int,
+    var url: String = ""
 )
