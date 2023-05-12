05209e69703ed2375c637403a899e00df7fbfb71
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-05-12 18:28:44 +0300

    add link to website

--- app/src/main/java/com/dev_talk/main/profile/information/ProfileLinkAdapter.kt
+++ app/src/main/java/com/dev_talk/main/profile/information/ProfileLinkAdapter.kt
@@ -1,8 +1,12 @@
 package com.dev_talk.main.profile.information
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.dev_talk.R
 import com.dev_talk.main.structures.Link
@@ -12,7 +16,8 @@ class ProfileLinkAdapter(val data: List<Link>) :
     RecyclerView.Adapter<ProfileLinkAdapter.LinkItemViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         LinkItemViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_link_profile, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_link_profile, parent, false),
+            parent.context
         )
 
     override fun getItemCount() = data.size
@@ -21,11 +26,17 @@ class ProfileLinkAdapter(val data: List<Link>) :
         holder.bind(data[position])
     }
 
-    class LinkItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    class LinkItemViewHolder(val view: View, val context: Context) : RecyclerView.ViewHolder(view) {
         private val icon = view.findViewById<CircleImageView>(R.id.links)
 
         fun bind(link: Link) {
             icon.setImageResource(link.icon)
+            icon.setOnClickListener {
+                val url = link.url
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(url)
+                context.startActivity(i)
+            }
         }
     }
 }
